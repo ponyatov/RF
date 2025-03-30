242a40cae404ea16617f9026365ff4e0203255a1
Dmitry Ponyatov <dponyatov@gmail.com>
2025-03-30 09:38:04 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
 use std::env;
+use std::fs::File;
+use std::io::prelude::*;
 
 fn main() {
     let argv: Vec<String> = env::args().collect();
     for (argc, argv) in argv.iter().skip(1).enumerate() {
         println!("argv[{}] = {}", argc, argv);
+        let mut file = File::open(argv).unwrap();
+        let mut src = String::new();
+        file.read_to_string(&mut src).unwrap();
+        println!("{}", src);
     }
 }
